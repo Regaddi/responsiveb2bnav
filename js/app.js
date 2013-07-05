c27f14a5726014e558a84c2164acb2e5d94936fa
Christian Brandt <christianbrandt90@gmail.com>
2013-07-05 13:16:14 +0200

    hammer.js. basic styling.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,36 @@
-$(function() {
-	$('.nav-toggle').on('click', function() {
-		$('nav').toggleClass('active');
+function initNav() {
+	var $nav = $('nav');
+	$('.nav-toggle').on('tap', function() {
+		$nav.toggleClass('active');
 	});
+	$('.main-container').hammer({
+		swipe_velocity: 0.1
+	}).on('swiperight', function(event) {
+		if(!$nav.is('.active')) {
+			event.gesture.preventDefault();
+			$nav.addClass('active');
+		}
+	})
+	.on('swipeleft', function(event) {
+		if($nav.is('.active')) {
+			event.gesture.preventDefault();
+			$nav.removeClass('active');
+		}
+	});
+	$('.subnav-btn', $nav).on('click', function() {
+		$(this).closest('ul').toggleClass('left');
+		$(this).siblings('ul').toggleClass('open');
+		$(this).closest('li').toggleClass('back');
+		$(this).children('i').toggleClass('icon-angle-circled-right icon-angle-circled-left');
+		if(!$(this).closest('li').is('.back')) {
+			$(this).siblings('ul').find('.back > .subnav-btn i').toggleClass('icon-angle-circled-right icon-angle-circled-left');
+			$(this).closest('li').find('.back, .left, .open').removeClass('back left open');
+		}
+	});
+}
+
+$(function() {
+	initNav();
+
+	fixie.init(["nav ul li a"]);
 });
